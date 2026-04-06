b2ca2aef9457e428151a8df9045b5c109f2491b5
Quan Do <137524513+qnd303@users.noreply.github.com>
2026-04-06 12:25:36 -0400

    Add: single-row-keyboard - submission-0

---
+++ Data Structures & Algorithms/single-row-keyboard/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        boom = {}
+        result = 0
+        for i, x in enumerate(keyboard):
+            if x not in boom:
+                boom[x] = i + 1
+        pre = boom[keyboard[0]]
+        for x in word:
+            result += abs(boom[x] - pre)
+            pre = boom[x]
+        return result

3122a452f605c1f7bbd9d7e7cf833a9ecb2b5927
Quan Do <137524513+qnd303@users.noreply.github.com>
2026-04-06 14:34:12 -0400

    Add: palindrome-permutation - submission-11

---
+++ Data Structures & Algorithms/palindrome-permutation/submission-11.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        bruh = {}
+        count = 0
+        allsame = set(s)
+
+        if len(s) == 1:
+            return True
+        
+        for x in s:
+            if x not in bruh:
+                bruh[x] = 1
+            else:
+                bruh[x] += 1
+
+        for x in bruh:
+            if bruh[x] % 2 == 1:
+                count += 1
+
+        if count > 1:
+            return False
+        return count < 2

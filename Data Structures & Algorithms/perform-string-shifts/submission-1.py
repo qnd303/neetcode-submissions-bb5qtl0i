700aea814794c442df0be5bdbabc4ddf7b70d209
Quan Do <137524513+qnd303@users.noreply.github.com>
2026-04-07 13:10:08 -0400

    Add: perform-string-shifts - submission-1

---
+++ Data Structures & Algorithms/perform-string-shifts/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        bro = []
+
+        for x in s:
+            bro.append(x)
+
+        for bruh in shift:
+            if bruh[0] == 0:
+                for x in range(bruh[1]):
+                    bah = bro.pop(0)
+                    bro.append(bah)
+            elif bruh[0] == 1:
+                for x in range(bruh[1]):
+                    bah = bro.pop()
+                    bro.insert(0, bah)
+        return "".join(bro)
